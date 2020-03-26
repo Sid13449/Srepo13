913293fed1ca11193744d300964a5ff09cfd1ccc
SID <SID@DESKTOP-6SGH93D>
2020-03-26 07:49:51 -0700

    Commit All

---
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -0,0 +1,13 @@
+package org.cap.model;
+
+public class SalesClass {
+	
+	int[] arr = {23,45,67,-10,87};
+	
+	public void print()
+	{
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
